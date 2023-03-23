1c2cc06f3fc73ab045cf41215a172c9e8816cf12
Eko Prastyo <604912+temon@users.noreply.github.com>
2023-03-24 04:23:09 +1100

    - init dashboard page using antd layout - convert the React project to support TS instead of JS only

--- web/app/src/App.js
+++ web/app/src/App.js
@@ -4,12 +4,13 @@ import LoginPage from './components/auth/LoginPage';
 import SignupPage from './components/auth/SignupPage';
 // import NotFoundPage from './components/common/NotFoundPage';
 import DashboardPage from './components/dashboard/DashboardPage';
+import Dashboard from "./components/layout/Dashboard";
 
 function App() {
   return (
       <Router>
         <Switch>
-          <Route exact path="/" component={LoginPage} />
+          <Route exact path="/" component={Dashboard} />
           <Route exact path="/signup" component={SignupPage} />
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/dashboard" component={DashboardPage} />
